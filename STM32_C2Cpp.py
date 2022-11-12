a6dbdd61f2110f465e4f0563007519f96e5a5ed1
Lorl0rd <palyilorinc33@gmail.com>
2022-11-12 15:01:54 +0100

    added README, corrected code (tests were successful)

--- STM32_C2Cpp.py
+++ STM32_C2Cpp.py
@@ -16,11 +16,17 @@ if __name__ == '__main':
     with open(path+filename_cpp, 'r') as f:
         cpp_lines = f.readlines()
 
+    should_copy=False
+    no_copy_user_code=False
+    same_lines=False
+    
     with open(path+filename_c, 'w') as fc:
         # write all generated code into main.c
         for c_line in c_lines:
-            fc.write(c_line)
+            # copy cpp lines after every USER CODE BEGIN statement
             if 'USER CODE BEGIN' in c_line:
+                fc.write(c_line) # write this line into file
+                no_copy_user_code=True # do not copy lines from c after USER CODE BEGIN
                 cpp_line2copy = []
                 for cpp_line in cpp_lines:
                     if c_line == cpp_line:
@@ -32,6 +38,11 @@ if __name__ == '__main':
                     if should_copy == True:
                         cpp_line2copy.append(cpp_line)
                 [fc.write(cpp_line2add) for cpp_line2add in cpp_line2copy]
+            # can copy lines after USER CODE END
+            if 'USER CODE END' in c_line:
+                no_copy_user_code=False
+            if not no_copy_user_code:
+                fc.write(c_line)
 
     # delete main.cpp then rename main.c to main.cpp
     os.remove(path+filename_cpp)
